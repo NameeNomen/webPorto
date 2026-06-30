9a39569cef2884d63f52d1c6dfcd057f614ca39a
NameeNomen <fat.lp3i33@gmail.com>
2026-06-30 16:55:09 +0700

    f

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from 'react';
 import { supabase } from './lib/supabase';
-
+import Link from 'next/link';
 export default function HomePage() {
   const [projects, setProjects] = useState<any[]>([]);
   const [lang, setLang] = useState<'native' | 'alt'>('native');
